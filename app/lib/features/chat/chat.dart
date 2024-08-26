bb75ecd04e8f4377270b49010444328c75f9455f
Awaddd <34748292+Awaddd@users.noreply.github.com>
2024-08-26 23:44:43 +0100

    scroll to bottom on load

--- app/lib/features/chat/chat.dart
+++ app/lib/features/chat/chat.dart
@@ -1,6 +1,7 @@
 import 'dart:developer';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:lmn/common/theme/constants.dart';
 import 'package:lmn/features/chat/chat_bubble.dart';
@@ -44,6 +45,18 @@ class MessagesView extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final user = ref.read(userProvider.notifier).user;
+    final ScrollController listViewController = ScrollController();
+
+    // find a way to have it load at the bottom instantly
+    SchedulerBinding.instance.addPostFrameCallback((_) {
+      Future.delayed(const Duration(milliseconds: 500)).then((value) {
+        listViewController.animateTo(
+          listViewController.position.maxScrollExtent,
+          duration: const Duration(milliseconds: 200),
+          curve: Curves.fastOutSlowIn,
+        );
+      });
+    });
 
     if (user == null) {
       log("user is empty");
@@ -57,6 +70,7 @@ class MessagesView extends ConsumerWidget {
     return Padding(
       padding: const EdgeInsets.all(md),
       child: ListView.separated(
+        controller: listViewController,
         itemBuilder: (context, index) {
           return ChatBubble(message: messages[index], author: user, recipient: recipient);
         },
