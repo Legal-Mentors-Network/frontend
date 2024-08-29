529bc80a51018c9942fc810f1314399a9fbcd696
Awaddd <34748292+Awaddd@users.noreply.github.com>
2024-08-29 19:12:57 +0100

    elevate send msg chatbox

--- app/lib/features/chat/send_message.dart
+++ app/lib/features/chat/send_message.dart
@@ -11,36 +11,40 @@ class SendMessage extends StatelessWidget {
   Widget build(BuildContext context) {
     final textColor = context.colors.onPrimaryContainer.withOpacity(0.7);
 
-    return Container(
-      padding: const EdgeInsets.symmetric(horizontal: lg, vertical: xs),
-      decoration: BoxDecoration(
-        color: context.colors.primaryContainer,
-        borderRadius: const BorderRadius.all(Radius.circular(rl)),
-      ),
-      child: Row(
-        children: [
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.only(left: md),
-              child: TextField(
-                cursorColor: textColor,
-                autocorrect: false,
-                style: TextStyle(color: context.colors.onPrimaryContainer),
-                enableInteractiveSelection: false,
-                decoration: InputDecoration(
-                  hintText: "Type a message",
-                  border: InputBorder.none,
-                  hintStyle: TextStyle(color: textColor),
+    return Material(
+      elevation: xs,
+      borderRadius: BorderRadius.circular(rl),
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: lg, vertical: xs),
+        decoration: BoxDecoration(
+          color: context.colors.primaryContainer,
+          borderRadius: const BorderRadius.all(Radius.circular(rl)),
+        ),
+        child: Row(
+          children: [
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.only(left: md),
+                child: TextField(
+                  cursorColor: textColor,
+                  autocorrect: false,
+                  style: TextStyle(color: context.colors.onPrimaryContainer),
+                  enableInteractiveSelection: false,
+                  decoration: InputDecoration(
+                    hintText: "Type a message",
+                    border: InputBorder.none,
+                    hintStyle: TextStyle(color: textColor),
+                  ),
                 ),
               ),
             ),
-          ),
-          IconButton(
-            icon: const Icon(Icons.send),
-            color: textColor,
-            onPressed: () {},
-          ),
-        ],
+            IconButton(
+              icon: const Icon(Icons.send),
+              color: textColor,
+              onPressed: () {},
+            ),
+          ],
+        ),
       ),
     );
   }
