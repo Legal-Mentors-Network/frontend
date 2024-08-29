2db876829bb49297b985f926ccb8a3eac06228a3
Awaddd <34748292+Awaddd@users.noreply.github.com>
2024-08-29 20:47:59 +0100

    add real time functionality

--- app/lib/state/message.dart
+++ app/lib/state/message.dart
@@ -9,7 +9,7 @@ import 'package:pocketbase/pocketbase.dart';
 final messageProvider = AsyncNotifierProvider.family<MessageNotifier, List<Message>, String>(MessageNotifier.new);
 
 class MessageNotifier extends FamilyAsyncNotifier<List<Message>, String> {
-  List<Message>? get messages => state.value;
+  List<Message> get messages => state.value ?? [];
 
   @override
   Future<List<Message>> build(String conversationId) async {
@@ -18,6 +18,8 @@ class MessageNotifier extends FamilyAsyncNotifier<List<Message>, String> {
     try {
       final filter = 'conversation = "$conversationId"';
       final messages = await pb.collection('messages').getList(page: 1, perPage: 30, filter: filter);
+
+      registerListener();
       return messages.items.map((message) => Message.fromNetwork(message)).toList();
     } on ClientException catch (e) {
       log("Failed to fetch messages ${e.statusCode} ${e.originalError}");
@@ -26,10 +28,20 @@ class MessageNotifier extends FamilyAsyncNotifier<List<Message>, String> {
     return [];
   }
 
-  Future<void> sendMessage({
-    required String conversationId,
-    required String message,
-  }) async {
+  Future<void> registerListener() async {
+    final pb = await ref.read(pocketbase);
+
+    pb.collection('messages').subscribe('*', (e) {
+      final record = e.record;
+
+      if (e.action == 'create' && record != null) {
+        final message = Message.fromNetwork(record);
+        state = AsyncData([...messages, message]);
+      }
+    });
+  }
+
+  Future<void> sendMessage({required String conversationId, required String message}) async {
     final pb = await ref.read(pocketbase);
     final user = ref.read(userProvider.notifier).user;
 
